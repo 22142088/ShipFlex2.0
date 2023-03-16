871ddbdd356900aeea34fc7bb716473c44368da1
Joren <Jorenbanens@hotmail.com>
2023-03-16 11:48:30 +0100

    changed menu to be static

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 class Menu{
 
-    public void printMenu(){
+    public static void printMenu(){
         System.out.println("1: offerte maken");
         System.out.println("2: klanten management");
         System.out.println("3: optie lijst");
@@ -29,8 +29,7 @@ class Menu{
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Menu menu = new Menu();
-        menu.printMenu();
+        Menu.printMenu();
         System.out.println("Kies een optie in het menu");
         int input1 = scanner.nextInt();
         System.out.println("u heeft gekozen voor optie " + input1);
