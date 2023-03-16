7f70625b4487f734a3e4414ab656fdec3ea4ce33
Joren <Jorenbanens@hotmail.com>
2023-03-16 11:00:13 +0100

    added Optie.java

---
+++ src/Optie.java
@@ -0,0 +1,20 @@
+public class Optie {
+    private String name;
+    private String description;
+    private int price;
+    Optie(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getPrice() {
+        return price;
+    }
+}
+
