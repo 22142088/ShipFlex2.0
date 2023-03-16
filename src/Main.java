5027c4955fb9fe44d37c157ea116cad4228424b2
Gandalf <michel-hut@outlook.com>
2023-03-16 11:46:48 +0100

    keuze check toegevoegd

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
 import java.util.Scanner;
+
+
 class Menu{
 
     public void printMenu(){
         System.out.println("1: offerte maken");
         System.out.println("2: klanten management");
         System.out.println("3: optie lijst");
-        System.out.println("3: optie lijst");
+    }
+
+    public static void checkKeuze(int keuze){
+        if(keuze == 1){
+            System.out.println("offerte maken");
+        }
+        else if (keuze == 2){
+            System.out.println("lijst met klanten");
+        }
+        else if (keuze == 3){
+            System.out.println("lijst met opties");
+        }
+        else {
+            System.out.println("geen geldige keuze");
+        }
     }
 
 }
@@ -18,5 +34,6 @@ public class Main {
         System.out.println("Kies een optie in het menu");
         int input1 = scanner.nextInt();
         System.out.println("u heeft gekozen voor optie " + input1);
+        Menu.checkKeuze(input1);
     }
 }
