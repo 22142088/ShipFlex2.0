b533fe62596894278ef1b5d4b299d770f4b18b73
Joren <Jorenbanens@hotmail.com>
2023-04-13 20:09:54 +0200

    made io in main private

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
-    public static IO io = new ConsoleIO();
+    private static IO io = new ConsoleIO();
     public static void main(String[] args) {
         OptieLijst.initialiseOptieLijst();
         new HoofdMenu().createMenu(io);
